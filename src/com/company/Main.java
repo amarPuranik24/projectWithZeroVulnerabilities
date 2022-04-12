775ebdce56a09af2dac095cbe54fa0979d1875db
amarPuranik24 <amar_puranik@persistent.com>
2022-04-12 20:54:37 +0530

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-      //  System.console().printf("Project with 0 vulnerabilities");
-      //  String password = "myPassword";
-      //  String encryptedpassword = null;
+        System.console().printf("Project with 0 vulnerabilities");
+        String password = "myPassword";
+        String encryptedpassword = null;
     }
 }
