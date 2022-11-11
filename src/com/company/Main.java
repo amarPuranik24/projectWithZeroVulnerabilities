18f3781b4704d5ae088956ec381954ba06ec6256
amarPuranik24 <amar_puranik@persistent.com>
2022-11-11 13:32:43 +0530

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,13 @@ public class Main {
     public static void main(String[] args) {
        System.console().printf("Project with 0 vulnerabilities");
         String password = "myPassword666";
+        String password1 = password;
         String encryptedpassword = null;
         System.console().printf("Project with 12 vulnerabilities");
         System.console().printf("Project with 14 vulnerabilities");
         System.console().printf("Project with 88 vulnerabilities");
         System.console().printf(password);
         System.console().printf(encryptedpassword);
+        System.console().printf(password1);
     }
 }
