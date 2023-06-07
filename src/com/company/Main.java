e5e1b2fa12cd659bbab953c6384a4db48e63e8d4
amarPuranik24 <88343631+amarPuranik24@users.noreply.github.com>
2023-06-07 10:44:41 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,6 @@ public class Main {
        System.console().printf("Project with 0 vulnerabilities");
         String password = "myPassword666";
         String password1 = password;
-        String password2 = "password81";
         String encryptedpassword = null;
         System.console().printf("Project with 12 vulnerabilities");
         System.console().printf("Project with 14 vulnerabilities");
